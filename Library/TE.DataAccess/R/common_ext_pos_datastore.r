830f43a5d5c658adf476a07124d260dc68a16e53
Lukasz Bednarz <lukasz.bednarz@reddeersystems.com>
2016-07-26 11:05:09 +0100

    Added to TE.DataAccess common_dealing_datastore.r common_event_datastore.r common_ext_pos_datastore.r common_factor_datastore.r

---
+++ Library/TE.DataAccess/R/common_ext_pos_datastore.r
@@ -0,0 +1,12 @@
+#' @include datastore.r global_configs.r
+NULL
+
+setClass(
+  Class          = "ExtPosDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('Strategy','Date'),unique_rows=TRUE),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@ext_psns_url,fields=c('strategy','start','end')),
+    key_map      = new("KeyMap",key_generator=date_strat_kgen_fn,key_columns=c('Strategy','Date'))
+  ), contains = c("DataStore")
+)
