3d4146e8188257018eb4955042ef51bc9ccbd133
Lukasz Bednarz <lukasz.bednarz@reddeersystems.com>
2016-07-26 11:15:38 +0100

    Added to TE.Data.Access : common_risk_factor_returns_datastore.r common_risk_instrument_exposure_datastore.r common_trade_levels_datastore.r common_trading_analysis_datastore.r common_watchlist_datastore.r

---
+++ Library/TE.DataAccess/R/common_risk_factor_returns_datastore.r
@@ -0,0 +1,12 @@
+#' @include datastore.r global_configs.r
+NULL
+
+setClass(
+  Class          = "RiskFactorReturnsDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('dtDateTime'), unique_rows = TRUE),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame",cstm_body_tag="prFactorRisk_GetFactorReturns_TraderAnalytics_Instrument"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@risk_fct_rtn_url,fields=c('start','end')),
+    key_map      = new("KeyMap",key_generator=date_only_kgen_fn,key_columns=c('dtDateTime'))
+  ), contains = c("DataStore")
+)
---
+++ Library/TE.DataAccess/R/common_trading_analysis_datastore.r
@@ -0,0 +1,42 @@
+#' @include datastore.r global_configs.r
+NULL
+
+setClass(
+  Class          = "TraderPerformanceDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('TraderID','DateTime')),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame",cstm_body_tag="DocumentElement"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@trader_perf_url,fields=c('id','start','end','mbam')),
+    key_map      = new("KeyMap",key_generator=date_trader_mbam_kgen_fn,key_columns=c('TraderID','DateTime','MBAMLevel'))
+  ), contains = c("DataStore")
+)
+
+setClass(
+  Class          = "PortfolioAnalysisDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('TraderID','DateTime')),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame",cstm_body_tag="DocumentElement"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@ratios_url,fields=c('id','start','end')),
+    key_map      = new("KeyMap",key_generator=date_trader_kgen_fn,key_columns=c('TraderID','DateTime'))
+  ), contains = c("DataStore")
+)
+
+setClass(
+  Class          = "TopPositionDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('TraderID','DateTime')),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame",cstm_body_tag="DocumentElement"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@top_psn_url,fields=c('id','start','end')),
+    key_map      = new("KeyMap",key_generator=date_trader_kgen_fn,key_columns=c('TraderID','DateTime'))
+  ), contains = c("DataStore")
+)
+
+setClass(
+  Class          = "BottomPositionDataStore",
+  prototype      = prototype(
+    dataset      = new("DataSet",key_cols=c('TraderID','DateTime')),
+    urlparser    = new("URLParser",parser_type = "XMLToFrame",cstm_body_tag="DocumentElement"),
+    urlquery     = new("URLQuery",root_url=middleware_urls@bottom_psn_url,fields=c('id','start','end')),
+    key_map      = new("KeyMap",key_generator=date_trader_kgen_fn,key_columns=c('TraderID','DateTime'))
+  ), contains = c("DataStore")
+)
