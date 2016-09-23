13f39aaebfffad7f79bb8a19ba8829aee8639e36
Guy Billings <guy.billings@reddeersystems.com>
2016-09-23 08:08:46 +0100

    Commit prior to making some fixes for TE meeting

--- R/scripts/build_analysis_block.r
+++ R/scripts/build_analysis_block.r
@@ -5,12 +5,13 @@ library(TE.AnalysisClasses)
 library(TE.FrontendEngine)
 
 load_only  <- FALSE
-block_name <- "PortfolioVarianceDecompositionAnalysisBlock"
-id         <- 11L
-date       <- '2016-08-01'
-fn         <- dated_three_monthly_lookback
+block_name <- "MarketStyleFactorStatisticAnalysisBlock"
+id         <- 11
+date       <- '2016-09-01'
+fn         <- dated_twelve_monthly_lookback
 
-b <- new(paste(block_name,"Client",sep=""))
+block_client <- new(paste(block_name,"Client",sep=""))
 keys <- fn(id, date)
 block_client <- dataRequest(block_client,keys,force=!load_only)
 data <- block_client@analysis_block@ggplot_data
+plot <- block_client@analysis_block@ggplot
