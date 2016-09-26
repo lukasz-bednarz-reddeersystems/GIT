20f84ab98c1e061866f4f4de176d20b78e00b271
Lukasz Bednarz <lukasz.bednarz@reddeersystems.com>
2016-09-26 18:33:59 +0100

    SQL scripts to generate tables for Remote Analysis Objectstore.

--- R/raid_tables/CREATE_tRDTE_AnalysisObjectstore.sql
+++ R/raid_tables/CREATE_tRDTE_AnalysisObjectstore.sql
@@ -1,11 +1,11 @@
 USE [FileTableDB]
 GO
 
-IF EXISTS (SELECT * FROM sysobjects WHERE name = 'tRDTE_PPModelObjectstore' AND type = 'U')
-		DROP TABLE [dbo].[tRDTE_PPModelObjectstore]
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'tRDTE_AnalysisObjectstore' AND type = 'U')
+		DROP TABLE [dbo].[tRDTE_AnalysisObjectstore]
 GO
 
-/****** Object:  Table [dbo].[tRDTE_PPModelObjectstore]    Script Date: 31/08/2016 14:14:05 ******/
+/****** Object:  Table [dbo].[tRDTE_AnalysisObjectstore]    Script Date: 31/08/2016 14:14:05 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -15,17 +15,17 @@ GO
 SET ANSI_PADDING ON
 GO
 
-CREATE TABLE [dbo].[tRDTE_PPModelObjectstore](
+CREATE TABLE [dbo].[tRDTE_AnalysisObjectstore](
     [bHashID] binary(64) NOT NULL,
 	[hPathLocator] hierarchyid NOT NULL,
 	[dtCreatedDate] datetime NOT NULL,
 	[sCreatedByUserID] varchar(50) NOT NULL,
-CONSTRAINT [AK_FileTableDB_PPModelObjectstore_HashIDPathLocator] UNIQUE NONCLUSTERED 
+CONSTRAINT [AK_FileTableDB_AnalysisObjectstore_HashIDPathLocator] UNIQUE NONCLUSTERED 
 (
 	[bHashID] ASC,
 	[hPathLocator] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY],
- CONSTRAINT [AK_FileTableDB_PPModelObjectstore_HashIDCreatedUser] UNIQUE NONCLUSTERED 
+ CONSTRAINT [AK_FileTableDB_AnalysisObjectstore_HashIDCreatedUser] UNIQUE NONCLUSTERED 
 (
 	[bHashId] ASC,
 	[dtCreatedDate] ASC,
@@ -38,12 +38,12 @@ GO
 SET ANSI_PADDING OFF
 GO
 
-ALTER TABLE [dbo].[tRDTE_PPModelObjectstore]  WITH CHECK ADD  CONSTRAINT [FK_ftRDTE_PPModelObjectstore_PathLocator] FOREIGN KEY([hPathLocator])
-REFERENCES [dbo].[ftRDTE_PPModelObjectstore] ([path_locator])
+ALTER TABLE [dbo].[tRDTE_AnalysisObjectstore]  WITH CHECK ADD  CONSTRAINT [FK_ftRDTE_AnalysisObjectstore_PathLocator] FOREIGN KEY([hPathLocator])
+REFERENCES [dbo].[ftRDTE_AnalysisObjectstore] ([path_locator])
 ON DELETE CASCADE
 GO
 
-ALTER TABLE [dbo].[tRDTE_PPModelObjectstore] CHECK CONSTRAINT [FK_ftRDTE_PPModelObjectstore_PathLocator]
+ALTER TABLE [dbo].[tRDTE_AnalysisObjectstore] CHECK CONSTRAINT [FK_ftRDTE_AnalysisObjectstore_PathLocator]
 GO
 
 
